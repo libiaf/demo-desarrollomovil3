78444f982b9153fee4eeb2299486946341b28ad9
libiaf <A01253425@tec.mx>
2025-11-11 14:34:25 -0700

    update

---
+++ src/db.js
@@ -0,0 +1,30 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export const query = (text, params) => {
+    return pool.query(text, params);
+};
+
+const initializeDatabase = async () => {
+    try {
+        await query (`
+        CREATE TABLE IF NOT EXISTS posts (
+            id SERIAL PRIMARY KEY,
+            content TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        `);
+        console.log('database tables created or already exist.');
+    } catch (err) {
+        console.error('Error initializing the database', err);
+    }
+}
+
+initializeDatabase();
+
+export default pool;
